314c49e7f39346be78e14aa4119f484bc3c642f6
Yubuhui <Yubuhui@atme.com>
2022-12-02 13:59:17 +0800

    first commit

---
+++ src/main/java/com/atme/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atme.git;
+
+/**
+ * @author 余不悔
+ * @date 2022/12/2
+ * @project git-test
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
